0ba2bf8a1f69d365dcc187d12d9e996830909e71
henriksaeten <hesae5115@student.uib.no>
2024-05-06 14:00:05 +0200

    Added the correct redirect-url environment file

--- src/environment.ts
+++ src/environment.ts
@@ -2,10 +2,10 @@ export const API_BASE_URL = 'https://test.unimicro.no/api/';
 
 export const OIDC_CONFIG = {
     authority: 'https://test-login.softrig.com',
-    client_id: '',
+    client_id: 'd966c686-aeca-48e5-819c-3df26a7a5f03',
     scope: 'AppFramework openid profile',
-    redirect_uri: location.origin,
-    post_logout_redirect_uri: location.origin,
+    redirect_uri: 'http://127.0.0.1:5173',
+    post_logout_redirect_uri: 'http://127.0.0.1:5173',
     silent_redirect_uri: location.origin,
 };
 
